cb1e4f20084e34f806e43a054689b0e67f95755a
Alongkorn Chetasumon <draood@gmail.com>
2019-06-29 13:21:34 +0700

    add info

--- go-gin-sample/school/go.mod
+++ go-gin-sample/school/go.mod
@@ -4,8 +4,10 @@ go 1.12
 
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/gbrlsnchs/jwt/v3 v3.0.0-beta.0
 	github.com/gin-gonic/gin v1.4.0
 	github.com/lib/pq v1.1.1
 	github.com/mattn/go-isatty v0.0.8 // indirect
+	github.com/stretchr/testify v1.3.0
 	golang.org/x/sys v0.0.0-20190602015325-4c4f7f33c9ed // indirect
 )
--- go-gin-sample/school/main.go
+++ go-gin-sample/school/main.go
@@ -1,13 +1,53 @@
 package main
 
 import (
+	"fmt"
+	"net/http"
+
+	"github.com/gbrlsnchs/jwt/v3"
 	"github.com/gin-gonic/gin"
 	_ "github.com/lib/pq"
 	"github.com/piggyman007/school/todo"
 )
 
 func main() {
+	hs256 := jwt.NewHMAC(jwt.SHA256, []byte("secret"))
+	h := jwt.Header{}
+	p := struct {
+		id int
+	}{
+		id: 1,
+	}
+	token, _ := jwt.Sign(h, p, hs256)
+	fmt.Printf("token = %s", token)
+
+	raw, _ := jwt.Parse(token)
+	if err := raw.Verify(hs256); err != nil {
+		fmt.Println("verify failed")
+	} else {
+		fmt.Println("verify success")
+	}
+
+	r := setupRouter()
+	r.Run(":1234")
+}
+
+func authMiddleware(c *gin.Context) {
+	token := c.GetHeader("Authorization")
+	if token != "Bearer token123" {
+		c.JSON(http.StatusUnauthorized, gin.H{"error": http.StatusText(http.StatusUnauthorized)})
+		c.Abort()
+		return
+	}
+
+	c.Next()
+}
+
+func setupRouter() *gin.Engine {
 	r := gin.Default()
+
+	r.Use(authMiddleware)
+
 	api := r.Group("/api")
 	{
 		api.POST("/todos", todo.Post)
@@ -15,7 +55,13 @@ func main() {
 		api.GET("/todos", todo.GetList)
 		api.PUT("/todos/:id", todo.UpdateByID)
 		api.DELETE("/todos/:id", todo.DeleteByID)
+		api.GET("/ping", func(c *gin.Context) {
+			fmt.Println(">> handler")
+			c.JSON(http.StatusOK, gin.H{
+				"hello": "world",
+			})
+		})
 	}
 
-	r.Run(":1234")
+	return r
 }
